52bd6b7af593e232ae935359468ad8c9ddd7e085
DESKTOP-QUST204\nqwrt <nqwrt@ymail.com>
2022-06-18 15:34:56 +0900

    first commit : hello world

---
+++ src/main/java/edu/hi/ex/SpringBootHjsExampleApplication.java
@@ -0,0 +1,13 @@
+package edu.hi.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHjsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHjsExampleApplication.class, args);
+	}
+
+}
