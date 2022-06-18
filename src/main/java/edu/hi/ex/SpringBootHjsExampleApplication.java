279578496e7cf8910865914dffe2ba6f0ea8967d
DESKTOP-QUST204\nqwrt <nqwrt@ymail.com>
2022-06-18 19:37:53 +0900

    시간테스트

--- src/main/java/edu/hi/ex/SpringBootHjsExampleApplication.java
+++ src/main/java/edu/hi/ex/SpringBootHjsExampleApplication.java
@@ -1,8 +1,10 @@
 package edu.hi.ex;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan(value = {"edu.hi.ex.mapper"})
 @SpringBootApplication
 public class SpringBootHjsExampleApplication {
 
