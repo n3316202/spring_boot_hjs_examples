6ac05659259d081e58aba24d2c13222db475fe9f
DESKTOP-QUST204\nqwrt <nqwrt@ymail.com>
2022-06-24 00:17:04 +0900

    commit

---
+++ src/main/java/edu/hi/ex/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package edu.hi.ex.config;
+
+//import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.WebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@EnableWebSecurity //스프링 시큐리티 필터가 스프링 필터체인에 등록됨
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+    public void configure(WebSecurity web) throws Exception {		
+		web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+}
