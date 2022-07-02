fbb28592b31af22b035f94d428a9ac629b9bad68
DESKTOP-QUST204\nqwrt <nqwrt@ymail.com>
2022-07-02 18:17:36 +0900

    commit

--- src/main/java/edu/hi/ex/config/SecurityConfig.java
+++ src/main/java/edu/hi/ex/config/SecurityConfig.java
@@ -53,8 +53,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers("/admin/**").hasAnyRole("ADMIN")
 		.antMatchers("/**").permitAll();
 		
-		http.formLogin(); //스프링 시큐리티에 있는 기본 로그인 폼을 사용하겠다.
-		
+		http.formLogin()
+			.loginPage("/login")	//loginPage() 는 말그대로 로그인할 페이지 url 이고
+			.permitAll(); //모든 유저가 로그인 화면은 볼 수 있게 한다
 	}
 	
 	
---
+++ src/main/java/edu/hi/ex/controller/LoginController.java
@@ -0,0 +1,17 @@
+package edu.hi.ex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class LoginController {
+
+	@GetMapping("/login")
+	public String home() {
+		return "login/login";
+	}
+
+}
